c15bd46e1b831b15a8dbb2e69b105fc523f1c5e0
ahmedalaatalaat <ahmedalaatalaat@gmail.com>
2023-02-20 14:09:47 +0200

    added image model

--- books_project/models.py
+++ books_project/models.py
@@ -11,3 +11,16 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+
+# to use images in github readme files 
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+    
+
+    class Meta:
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+
+    def __str__(self):
+        return str(self.upload_date)
